1c9218aa19d96f3e3a971084fe35da40be88b4bf
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-10 06:40:19 +0000

    done

---
+++ dom-manipulation/script.js
@@ -0,0 +1,34 @@
+const quoteDisplay = document.getElementById('quote-display');
+
+const quotes = [
+    {quote: "The only way to do great work is to love what you do.", category: "Motivation"},
+    {quote: "Those who want leader the orchestra must be willing to turn their back on the crowd.", category: "Leadership"},
+    {quote: "In the middle of every difficulty lies opportunity.", category: "Inspiration"},
+    {quote: "Success usually comes to those who are too busy to be looking for it.", category: "Success"},
+    {quote: "Don't watch the clock; do what it does. Keep going.", category: "Time Management"},
+]
+function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    quoteDisplay.textContent = `"${randomQuote.quote}" - "${randomQuote.category}"`;
+}
+document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+showRandomQuote(); // show a quote when the page loads
+
+function AddQuoteForm(){
+    const newQuoteText = document.getElementById('newQuoteText').value;
+    const newQuoteCategory = document.getElementById('newQuoteCategory').value;
+    if(newQuoteText && newQuoteCategory){
+        quotes.push({quote: newQuoteText, category: newQuoteCategory});
+        document.getElementById('newQuoteText').value = '';
+        document.getElementById('newQuoteCategory').value = '';
+        alert('New quote added successfully!');
+    } else {
+        alert('Please enter both quote and category.');
+
+    }
+}
+function addQuote() {
+    AddQuoteForm();
+}
+document.getElementById('addQuote').addEventListener('click', addQuote);
